ab64e8fe8dd6d7d6faef926ad2edda01906d4e73
olstrike <oliverinacka@gmail.com>
2022-02-15 15:06:29 +0100

    Tog nicklas kod för att jag inte kunde se längst nere på skärmen

--- sak/recouses.py
+++ sak/recouses.py
@@ -1,10 +1,43 @@
-class character:
-
-    def __init__(self, name, health, attack, armor) -> None:
+class Character:
+    
+    def __init__(self, name, health, attack, armor):
         self.name = name
         self.health = health
         self.attack = attack
         self.armor = armor
-    
+        
     def __str__(self) -> str:
-        return f"name: {self.name}\n {self.health}\n {self.attack}\n {self.armor}"
+        return f"Name: {self.name}\n health: {self.health}\n attack: {self.attack}\n armor: {self.armor}"
+    
+    def attack(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
+
+class Goblin:
+    
+    def __init__(self, health, attack, armor):
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+        
+    def __str__(self) -> str:
+        return f"Goblin\n health: {self.health}\n attack: {self.attack}\n armor: {self.armor}"
+
+    def attack(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
+    
