eecc92da489a7b15ae4a0fad4f5bce11280d212d
olstrike <oliverinacka@gmail.com>
2022-02-16 15:07:42 +0100

    Gort om så gott som hela skiten

--- sak/recources.py
+++ sak/recources.py
@@ -9,26 +9,31 @@ class Character:
     def __str__(self) -> str:
         return f"Name: {self.name}\n health: {self.health}\n attack: {self.attack}\n armor: {self.armor}"
     
-    def attack(self):
-        return self.attack
     
     def take_damage(self, damage):
         relative_damage = damage - self.armor
         self.health -= relative_damage
         if self.health < 0: self.health = 0
 
+    def get_attack(self):
+        return self.attack
+
     def get_health(self):
         return self.health
+
+    def get_name(self):
+        return self.name
 
 class Goblin:
     
-    def __init__(self, health, attack, armor):
+    def __init__(self, health, attack, armor, id):
         self.health = health
         self.attack = attack
         self.armor = armor
+        self.id = id
         
     def __str__(self) -> str:
-        return f"Goblin\n health: {self.health}\n attack: {self.attack}\n armor: {self.armor}"
+        return f"Goblin #{self.id}\n health: {self.health}\n attack: {self.attack}\n armor: {self.armor}"
 
     def attack(self):
         return self.attack
@@ -40,4 +45,10 @@ class Goblin:
 
     def get_health(self):
         return self.health
+
+    def get_attack(self):
+        return self.attack
+    
+    def get_name(self):
+        return f"Goblin #{self.id}"
     
